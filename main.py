d46c0130277e067ac4df5f73c6fd55dce34d5f94
felixfuchs4 <felixfuchs4@outlook.de>
2025-11-25 18:48:54 +0100

    added Player.py, circleshape.py; appended constants.py and main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pygame
 from constants import SCREEN_HEIGHT,SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock=pygame.time.Clock()
+    dt=0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    player1= Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
     while True:
         log_state()
@@ -15,7 +19,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
+
+        dt=clock.tick(60) / 1000
+        #print(dt)
+
+
 
 
 if __name__ == "__main__":
